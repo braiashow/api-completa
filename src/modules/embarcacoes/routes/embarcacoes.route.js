db81cda84142822b16767b3915ecd97970135344
braiashow <braiashow@gmail.com>
2026-07-22 16:55:50 -0300

    criando admins

--- src/modules/embarcacoes/routes/embarcacoes.route.js
+++ src/modules/embarcacoes/routes/embarcacoes.route.js
@@ -7,7 +7,7 @@ router.get("/listar", EmbarcacoesController.listarTodos);
 
 router.post("/cadastrar", EmbarcacoesController.cadastrar);
 
-router.put("/atualizar/total/:numPat", EmbarcacoesController.atualizarTotal);
+router.put("/atualizar/total", EmbarcacoesController.atualizarTotal);
 
 router.patch("/atualizar/parcial/:numPat", EmbarcacoesController.atualizarParcial);
 
